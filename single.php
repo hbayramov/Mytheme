44a47db1d2c3e12fdb4ea9bd03ae0cbf87d39fa7
Huseyn <hsyn.bayramov@gmail.com>
2015-08-24 20:27:57 +0500

    almost nothing changed

--- single.php
+++ single.php
@@ -3,13 +3,13 @@
 
 if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
-	
-	 <div class="single">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	  
+   <article class="page">
+		<h2><?php the_title(); ?></h2>
 		<?php the_content(); ?>
-	  </div>
+	  </article>  
 	
-	<?php endwhile;
+   <?php endwhile;
 	
 	else :
 		echo '<p>No content found</p>';
