1df06bc0d3d9143a8f03084db6a34fd7a76fd1a3
Huseyn <hsyn.bayramov@gmail.com>
2015-08-24 20:26:47 +0500

    footer removed

--- header.php
+++ header.php
@@ -8,16 +8,20 @@
 	</head>
 	
 <body <?php body_class(); ?>>
-	
-	 <div class="container">
-	
-		<!-- site-header -->
+    <!-- site-header -->
 		<header class="site-header">
-      <h1><a href="<?php echo home_url(); ?>" style="text-decoration:none;"><?php bloginfo('name'); ?></a></h1>
-      <p><?php bloginfo('description');  ?><p>
-      <p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
-      <p>Maraqlı gələn proekt olsa <a href="https://github.com/hbayramov">Fork</a>'la.  </p>
-      <p> Mənə <a href="mailto:hsyn.bayramov@gmail.com" style="text-decoration:none;"> e-mail </a>  göndər. </p>
-     </header><!-- /site-header -->
+        <h1><a href="<?php echo home_url(); ?>" style="text-decoration:none;"><?php bloginfo('name'); ?></a></h1>
+        <p><?php bloginfo('description');  ?> <p>
+        <p> Bu mənim şəxsi blog səhifəmdir. </p>
+        <p> Kompüter Mühəndisi (Hələ ki məzun ola bilməmişəm.)	</p>
+        <p> Programlama, Programlama Yarışları (Competitive Programming).</p>
+        <p> Web (Javascript'i səndə sevməlisən.)  </p>
+        <div class="extra_info"> 
+       		<p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
+        	<p>Maraqlı gələn proekt olsa <a href="https://github.com/hbayramov">Fork</a>'la.  </p>
+        	<p> Mənə <a href="mailto:hsyn.bayramov@gmail.com" style="text-decoration:none;"> e-mail </a>  göndər. </p>
+      </div>
+      </header><!-- /site-header -->  
+  </body>
 
-  
+</html>
