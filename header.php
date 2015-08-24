9b58463a59882a675abda957e0d51ede2f380c73
Huseyn <hsyn.bayramov@gmail.com>
2015-08-24 12:27:33 +0500

    header added

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width">
+		<title><?php bloginfo('name'); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	
+<body <?php body_class(); ?>>
+	
+	 <div class="container">
+	
+		<!-- site-header -->
+		<header class="site-header">
+      <h1><a href="<?php echo home_url(); ?>" style="text-decoration:none;"><?php bloginfo('name'); ?></a></h1>
+      <p><?php bloginfo('description');  ?><p>
+      <p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
+      <p>Maraqlı gələn proekt olsa <a href="https://github.com/hbayramov">Fork</a>'la.  </p>
+      <p> Mənə <a href="mailto:hsyn.bayramov@gmail.com" style="text-decoration:none;"> e-mail </a>  göndər. </p>
+     </header><!-- /site-header -->
+
+  
