2a8e1f2bb069cca09cbf644e0b38979ef5b23a34
Huseyn <hsyn.bayramov@gmail.com>
2015-08-25 18:00:08 +0500

    widgets added

--- functions.php
+++ functions.php
@@ -7,3 +7,13 @@ function learningWordPress_resources() {
 }
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
+
+function WidgetsInit() {
+    
+	register_sidebar(array (
+	     'name' => 'Sidebar',
+		    'id' => 'sidebar1'
+	));
+}
+
+add_action('widgets_init','WidgetsInit');
