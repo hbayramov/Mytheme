56721b843e40cd10da6b39d7b0fc331f72b61b75
Huseyn <hsyn.bayramov@gmail.com>
2015-08-28 09:39:23 +0500

    linkedin link added

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta name="viewport" content="width=device-width">
-		<title><?php bloginfo('name'); ?></title>
-		<?php wp_head(); ?>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?>  | Ana Səhifə </title>
+    <?php wp_head(); ?>
 	</head>
 	
 <body <?php body_class(); ?>>
@@ -13,7 +13,7 @@
         <h1><a href="<?php echo home_url(); ?>" style="text-decoration:none;"><?php bloginfo('name'); ?></a></h1>
           <p><?php bloginfo('description');  ?> </p>
        		 <p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
-      		 <p> <a href="#">bio</a></p>
+      		 <p><a href="https://tr.linkedin.com/pub/huseyn-bayramov/81/914/883"> LinkedIn</a></p>
   </header><!-- /site-header -->  
   
   </body>
