35866b32defabf132a2d1278c0e5824f4f265e0b
hbayramov <hsyn.bayramov@gmail.com>
2015-10-31 22:15:03 +0300

    icons added

--- header.php
+++ header.php
@@ -1,21 +1,46 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
+	 <head>
+		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?>  | Ana Səhifə </title>
     <?php wp_head(); ?>
 	</head>
 	
 <body <?php body_class(); ?>>
+     <?php include_once("analyticstracking.php") ?>
+      <a href="https://github.com/hbayramov" target="_blank"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a> 
     <!-- site-header -->
 		<header class="site-header">
-        <h1><a href="<?php echo home_url(); ?>" style="text-decoration:none;"><?php bloginfo('name'); ?></a></h1>
-          <p><?php bloginfo('description');  ?> </p>
-       		 <p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
-      		 <p><a href="https://tr.linkedin.com/pub/huseyn-bayramov/81/914/883"> LinkedIn</a></p>
+          <h1 class="logo"><a href="<?php echo home_url(); ?>" style="text-decoration:none;"> <?php bloginfo('name');?> </a></h1>
+          <div class="descrp">
+             	<p><?php bloginfo('description');  ?> </p>
+              <p><a href="https://www.goodreads.com/user/show/7796735-bayramov-huseyn">Goodreads</a>'də dost ola bilərik. </p>
+      		    <p><a href="https://tr.linkedin.com/pub/huseyn-bayramov/81/914/883">LinkedIn</a>
+               profilim</p>
+             <div class="image">
+               <p  style="margin-left:48px;"> 
+                      <img src = "http://huseyn.org/wp-content/uploads/2015/09/man.png" alt ="man">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/home.png" alt ="home">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/happy2.png" style="margin-left:10px;" alt="happy">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/food.png" style="margin-left:10px;" alt = "food">
+               <p style="margin-top:10px">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/gamepad.png" style="margin-left:2px;" alt = "gamepad">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/github.png" style="margin-left:2px;" alt = "github">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/embed2.png" alt="embed2">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/apple.png" style="margin-left:4px;" alt="apple">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/connection.png" style="margin-left:5px;" alt="connection">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/keyboard.png" style="margin-left:4px;" alt="keyboard">  </p>
+               <p style="margin-left:48px; margin-top:-2px;">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/firefox.png" alt="firefox">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/html5.png" style="margin-left:5px;" alt="html5">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/tux.png" style="margin-left:5px;" alt="tux">
+                       <img src = "http://huseyn.org/wp-content/uploads/2015/09/refresh.png" style="margin-left:5px;" alt="refresh"> </p>
+            </div>
+      </div>
   </header><!-- /site-header -->  
-  
+   
   </body>
 
 </html>
+
