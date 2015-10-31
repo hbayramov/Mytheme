4815318d3d2850c1a03ddb00bebddeb4095909c4
hbayramov <hsyn.bayramov@gmail.com>
2015-10-31 22:14:42 +0300

    widgets added

--- functions.php
+++ functions.php
@@ -11,9 +11,18 @@ add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 function WidgetsInit() {
     
 	register_sidebar(array (
-	     'name' => 'Sidebar',
+	     'name' => 'Sidebar1',
 		    'id' => 'sidebar1'
+	));
+  register_sidebar(array (
+	     'name' => 'Sidebar2',
+		    'id' => 'sidebar2'
+	));
+   register_sidebar(array (
+	     'name' => 'Sidebar3',
+		    'id' => 'sidebar3'
 	));
 }
 
-add_action('widgets_init','WidgetsInit');
+add_action('widgets_init','WidgetsInit'); ?>
+
